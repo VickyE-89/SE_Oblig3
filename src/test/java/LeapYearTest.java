1303fc448782bcbc151a534e2e17867ff5145a0a
VickyE-89 <VickyE-89@users.noreply.github.com>
2021-10-27 15:28:35 +0200

    Lagt til filer fra Oblig 2

---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LeapYearTest {
+
+    @Test
+    public void if_year_modulo_400() {
+        assertTrue(LeapYear.isLeapYear(2000));
+    }
+
+    @Test
+    public void if_year_modulo_4_and_modulo_not_100() {
+        assertTrue(LeapYear.isLeapYear(2024));
+    }
+
+    @Test
+    public void if_year_modulo_not_4() {
+        assertFalse(LeapYear.isLeapYear(1754));
+    }
+
+    @Test
+    public void if_year_modulo_100_and_modulo_not_400() {
+        assertFalse(LeapYear.isLeapYear(170));
+    }
+
+}
